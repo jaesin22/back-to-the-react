b749997f518fa6da138ac5e6b23dcf5925645253
Jaesin22 <jaesin22@naver.com>
2022-07-11 01:54:07 +0900

    2021-07-10 react

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import Button from './Button'
+import "./styles.css"
+
 function App() {
   return (
     <div>
       <h1>back to the react</h1>
+      <Button text={"continue"} />
     </div>
   );
 }
