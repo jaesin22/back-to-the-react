ba4bb665e393bdbe081af9502e5125beaf3eecd9
Jaesin22 <jaesin22@naver.com>
2022-07-12 23:55:11 +0900

    2021.07.12 useEffct

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
+import { useState, useEffect } from 'react';
 import Button from './Button'
 
 function App() {
+  const [counter, setValue] = useState(0)
+  const [keyword, setKeyword] = useState("")
+  const onClick = () => setValue((prev) => prev + 1)
+  const onChange = (event) => setKeyword(event.target.value)
+  console.log("i run all the time")
+
+  useEffect(() => {
+    console.log("I run only once")
+  }, [])
+  useEffect(() => {
+      console.log("I run when 'keyword' changes.")
+  }, [keyword])
+  useEffect(() => {
+      console.log("I run when 'counter' changes.")
+  }, [counter])
+
   return (
     <div>
-      <h1>back to the react</h1>
-      <Button text={"continue"} />
+      <input value={keyword} type="text" onChange={onChange} placeholder="search"/>
+      <h1>{counter}</h1>
+      <button onClick={onClick}>Click Me</button>
     </div>
   );
 }
